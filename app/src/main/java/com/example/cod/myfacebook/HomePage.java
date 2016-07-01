e1046bd65cdfb79cb821e946ee9b7f8a5059453a
SOBIN JOSE <SOBIN JOSE>
2016-07-01 13:05:57 +0530

    sprint 1

---
+++ app/src/main/java/com/example/cod/myfacebook/HomePage.java
@@ -0,0 +1,41 @@
+package com.example.cod.myfacebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class HomePage extends AppCompatActivity {
+    EditText ed1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        ed1=(EditText) findViewById(R.id.editText);
+        ed1.setText("Hello world");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
